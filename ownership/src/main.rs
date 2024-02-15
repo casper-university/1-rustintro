c7f672011fa00d71d796ef26bf4fd293d26f78e2
Mel Padden <paddentech@gmail.com>
2024-02-15 13:17:15 +0100

    ownership demonstration

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let s = String::from("hello");  // s comes into scope
     takes_ownership(s);             // s's value moves into the function…
                                     // ... and so is no longer valid here
+    println!("{}", s); // this will not compile
     let x = 5;                      // x comes into scope
     makes_copy(x);                  // x would move into the function,
                                     // but i32 is Copy, so it's okay to still
