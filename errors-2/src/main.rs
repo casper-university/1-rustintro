8c96e2e838a22c24b08ec272b8d25d322626f3a3
Mel Padden <paddentech@gmail.com>
2024-02-15 13:00:36 +0100

    End of lecture

--- errors-2/src/main.rs
+++ errors-2/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
-    let greeting_file = match greeting_file_result {
+    let _greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
--- traits-1/src/main.rs
+++ traits-1/src/main.rs
@@ -1,32 +1,34 @@
-use aggregator::{Summary, Tweet, NewsArticle};
+use aggregator::{NewsArticle, Summary, Tweet};
 mod aggregator;
 
 fn main() {
     let article = NewsArticle {
         headline: String::from("Literate horse amazes onlookers"),
-        content: String::from(
-            "A literal horse is literally literate.",
-        ),
+        content: String::from("A literal horse is literally literate."),
         location: String::from("Dublin"),
         author: String::from("Mel Padden"),
     };
-    
+
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people...",
-        ),
+        content: String::from("of course, as you probably already know, people..."),
         reply: false,
         retweet: false,
     };
 
-    println!("1 new tweet: {}", tweet.summarize());
-    println!("1 new article: {}", article.summarize());
+    //println!("1 new tweet: {}", tweet.summarize());
+    //println!("1 new article: {}", article.summarize());
 
-    summarize_generic(&tweet);
-    summarize_generic(&article);
+    //summarize(&tweet);
+    //summarize(&article);
+    summarize_list(vec! [ &tweet, &article ] );
 }
 
-fn summarize_generic(item: &dyn Summary) {
+fn summarize(item: &dyn Summary) {
     println!("1 new article: {}", item.summarize());
+}
+fn summarize_list(items: Vec<&dyn Summary>) {
+    for item in items.into_iter() {
+        summarize(item);
+    }
 }
